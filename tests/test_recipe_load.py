3a796f7da83ad93d86efaeb5820b03c34598efab
Cody Baker <codycbakerphd@gmail.com>
2022-03-14 21:00:46 -0400

    added tests

--- tests/test_recipe_load.py
+++ tests/test_recipe_load.py
@@ -2,6 +2,7 @@ import jsonschema
 import json
 from unittest import TestCase
 from pathlib import Path
+from jsonschema import ValidationError
 
 from como_recipes import load_recipe
 
@@ -11,13 +12,21 @@ class TestRecipeLoad(TestCase):
     def setUpClass(cls):
         cls.recipe_paths = Path(__file__).parent.parent / "como_recipes"
 
+        with open(file=Path(__file__).parent.parent / "recipe_schema.json", mode="r") as fp:
+            cls.recipe_schema = json.load(fp=fp)
+
     def test_recipe_load(self):
         for file_path in (Path(__file__).parent.parent / "como_recipes" / "recipes").iterdir():
             if ".md" in file_path.suffixes:
                 recipe = load_recipe(file_path=file_path)
                 recipe.ingredients = [x.__dict__ for x in recipe.ingredients]  # for schema validation
                 recipe.instructions = ""
-                recipe = recipe.__dict__
-                with open(file=Path(__file__).parent.parent / "recipe_schema.json", mode="r") as fp:
-                    schema = json.load(fp=fp)
-                jsonschema.validate(instance=recipe, schema=schema)
+                jsonschema.validate(instance=recipe.__dict__, schema=self.recipe_schema)
+
+    def test_schema_bad_recipe(self):
+        with self.assertRaises(expected_exception=ValidationError):
+            jsonschema.validate(instance=dict(WRONG=dict()), schema=self.recipe_schema)
+
+    def test_schema_bad_ingredient(self):
+        with self.assertRaises(expected_exception=ValidationError):
+            jsonschema.validate(instance=dict(name="test", ingredients=[dict(WRONG=1)]), schema=self.recipe_schema)
