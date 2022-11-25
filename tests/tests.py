a0a9a464f9a66f892d7024fb6818fee072f8ea0b
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-11-24 22:36:35 -0500

    Delete tests.py

--- tests/tests.py
+++
@@ -1,9 +0,0 @@
-"""Test recipe loading."""
-from pathlib import Path
-from unittest import TestCase
-
-
-class TestRecipeLoad(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.recipe_paths = Path(__file__).parent.parent / "como_recipes"
