08218cfe335c3b08db7ccaee28ed4597ea3db58d
Cody Baker <cody.c.baker.phd@gmail.com>
2024-11-04 15:43:03 -0500

    debug

--- tests/examples/example_1/_example_recipe_1.py
+++ tests/examples/example_1/_example_recipe_1.py
@@ -4,8 +4,8 @@ from como_recipes import Recipe, MeasuredIngredient, default_recipe_registry
 class ExampleRecipe1(Recipe):
     name: str = "Example Recipe 1"
     ingredients: list[MeasuredIngredient] = [
-        MeasuredIngredient(name="ingredient 1", amount=3, unit="tbsp."),
-        MeasuredIngredient(name="ingredient 2", amount=4, unit="g"),
+        MeasuredIngredient(name="ingredient 1", amount=3.0, unit="tbsp."),
+        MeasuredIngredient(name="ingredient 2", amount=4.0, unit="g"),
     ]
     instructions: list[str] = [
         "This is an example of a recipe.",
