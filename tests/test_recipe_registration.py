d3193f9d071ada1ba65b4da01a63ed71e76a1d7b
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-11-07 12:53:04 -0500

    Enhance registry printout (#257)
    * enhance measure registry printout
    
    * enhance measure registry printout

--- tests/test_recipe_registration.py
+++ tests/test_recipe_registration.py
@@ -24,11 +24,12 @@ def test_example_recipe_1_add_to_registry():
 
     new_registry.add_recipe(recipe=recipe)
 
+    expected_repr = "1 registered recipes\n--------------------\n\nExample Recipe 1\n"
     assert len(new_registry) == 1
-    assert repr(new_registry) == "1 registered recipes\n--------------------\n\nExample Recipe 1\n"
+    assert repr(new_registry) == expected_repr
     with patch("sys.stdout", new=StringIO()) as captured_output:
         print(new_registry)
-    assert captured_output.getvalue() == "1 registered recipes\n--------------------\n\nExample Recipe 1\n\n"
+    assert captured_output.getvalue() == expected_repr + "\n"
     assert new_registry.get_recipe(recipe_name="Example Recipe 1") == recipe
 
 
