fa73ffd521c0b31ce62d367d8acb1a3f187a8cac
Cody's Laptop <cody.c.baker.phd@gmail.com>
2024-11-07 22:07:24 -0500

    continue work on CLI

--- src/como_recipes/_command_line_interface.py
+++ src/como_recipes/_command_line_interface.py
@@ -3,6 +3,7 @@
 import pathlib
 import click
 import importlib
+import datetime
 
 from ._base_recipe import Recipe
 from ._recipe_registration import default_recipe_registry
@@ -83,56 +84,67 @@ def _write_missing_pydantic_recipes(*, limit: int | None = None) -> None:  # pra
 @click.command(name="select_recipes")
 def _select_recipes() -> None:
     """Interactively select available recipes and generate a shopping list for them."""
+    home_folder = pathlib.Path.home() / ".como_recipes"
+    home_folder.mkdir(exist_ok=True)
+    session_file_path = home_folder / f"session_{datetime.datetime.now().strftime('%Y%m%d')}.txt"
+    counter = 0
+    while session_file_path.exists():
+        session_file_path = home_folder / f"session_{datetime.datetime.now().strftime('%Y%m%d')}_{counter}.txt"
+        counter += 1
+
     recipe_selection = MeasurementRegistry()
 
-    default_recipe_integer_map: dict[int, str] = {
+    id_to_default_recipe_name_map: dict[int, str] = {
         selection_value: recipe_name
         for selection_value, recipe_name in enumerate(default_recipe_registry.get_all_recipe_names())
     }
-    default_recipe_full_name_map: dict[str, bool] = {
-        recipe_name: True for recipe_name in default_recipe_registry.get_all_recipe_names()
-    }
+    default_recipe_name_to_id_map: dict[str, int] = {value: key for key, value in id_to_default_recipe_name_map.items()}
 
     start_message = (
         "\n\nWelcome to the CoMo Recipes Meal Selection Interface!\n\n"
-        "To exit, enter 'q'.\n"
-        "To see all available recipes and their identifiers (IDs), enter 'l'.\n"
-        "To add a recipe to the selection, enter the corresponding ID or type its full name.\n"
-        "To remove a recipe from the selection, enter 'r' followed by the ID or its full name.\n"
-        "To see the list of currently selected recipes, enter 'c'.\n"
-        "To see this command-line options menu again, enter 'm'.\n\n"
+        "To exit, enter 'q' or 'quit'.\n"
+        "To see all available recipes and their identifiers (IDs), enter 'lav' or 'list available'.\n"
+        "To see the list of current recipe selections, enter 'lcs' or 'list current selection'.\n"
+        "To add a recipe to the current selection, enter 'a' or 'add'.\n"
+        "To remove a recipe from the current selection, enter 'r' or 'remove'.\n"
+        "To get the shopping list for the current recipe selection, enter 'gsl' or 'get shopping list'.\n"
+        "To see these start menu options again, enter 'sm' or 'start menu'.\n\n"
     )
     click.echo(message=start_message)
 
+    input_value = ""
     unrecognized_input_message = (
-        "Input for '{input_value}' is not recognized. Please try again.\n"
-        "To see the command-line options menu, enter 'm'\n"
+        f"Input for '{input_value}' is not recognized. Please try again.\n"
+        "To see the command-line options menu, enter 'sm' or 'start menu'.\n"
     )
 
     max_iterations = 1_000_000
     iteration = 0
     while iteration < max_iterations:
-        input_value = click.prompt(text="", prompt_suffix="", type=str)
+        input_value = click.prompt(text="What would you like to do?: ", prompt_suffix="", type=str)
 
         match input_value:
-            case "q":
+            case "q" | "quit":
                 break
-            case "m":
+            case "m" | "menu":
                 click.echo(message=start_message)
-            case "l":
+            case "lav" | "list available":
                 click.echo(message="\nAvailable recipes:\n")
                 message = ""
-                for input_value, recipe_name in default_recipe_integer_map.items():
+                for input_value, recipe_name in id_to_default_recipe_name_map.items():
                     message += f"{input_value}) {recipe_name}\n"
                 message += "\n\n"
                 click.echo(message=message)
-            case "c":
-                click.echo(message=f"\nCurrently selected recipes:\n{recipe_selection.get_all_recipe_names()}")
-            case "a":
-                input_value = click.prompt(text="Recipe to add: ", prompt_suffix="", type=str)
+            case "lsc" | "list current selection":
+                recipe_names_with_ids = ""
+                for recipe_name in recipe_selection.get_all_recipe_names():
+                    recipe_names_with_ids += f"{recipe_name} ({default_recipe_name_to_id_map[recipe_name]})\n"
+                click.echo(message=f"\nCurrently selected recipes: \n{recipe_names_with_ids}\n\n")
+            case "a" | "add":
+                input_value = click.prompt(text="Recipe ID or name to add: ", prompt_suffix="", type=str)
                 if input_value.isdigit():
-                    recipe_name = default_recipe_integer_map[int(input_value)]
-                elif default_recipe_full_name_map.get(input_value, False) is True:
+                    recipe_name = id_to_default_recipe_name_map[int(input_value)]
+                elif default_recipe_name_to_id_map.get(input_value, False) is not False:
                     recipe_name = input_value
                 else:
                     click.echo(message=unrecognized_input_message)
@@ -140,20 +152,33 @@ def _select_recipes() -> None:
 
                 recipe_selection.add_recipe(recipe=default_recipe_registry.get_recipe(recipe_name=recipe_name))
                 click.echo(message=f"\nRecipe '{recipe_name}' added to the list.\n")
-            case "r":
-                input_value = click.prompt(text="Recipe to add: ", prompt_suffix="", type=str)
+            case "r" | "remove":
+                input_value = click.prompt(text="Recipe ID or name to remove: ", prompt_suffix="", type=str)
                 if input_value.isdigit():
-                    recipe_name = default_recipe_integer_map[int(input_value)]
-                elif default_recipe_full_name_map.get(input_value, False) is True:
+                    recipe_name = id_to_default_recipe_name_map[int(input_value)]
+                elif default_recipe_name_to_id_map.get(input_value, False) is not False:
                     recipe_name = input_value
                 else:
                     click.echo(message=unrecognized_input_message)
                     continue
 
                 pass  # TODO: Implement recipe removal
+            case "gsl" | "get shopping list":
+                shopping_list = recipe_selection.get_shopping_list()
+                click.echo(message=shopping_list)
+
+                if click.prompt(text="Would you like to save this shopping list to a file? (y/n): ", type=str) != "y":
+                    continue
+
+                with open(file_path=session_file_path, mode="w") as io:
+                    io.write(shopping_list)
+                click.launch(url=session_file_path, locate=True)
             case _:
                 click.echo(message=unrecognized_input_message)
 
         iteration += 1
+
+    if iteration >= max_iterations:
+        click.echo(message="Exiting the CoMo Recipes Meal Selection Interface: maximum allowed operations reached.\n")
 
     return None
