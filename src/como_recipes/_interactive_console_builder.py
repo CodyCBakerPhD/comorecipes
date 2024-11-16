0248930277d2741e20cd7992fc676a6508f51091
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-11-15 20:14:48 -0500

    Debug deployment workflow (#266)
    * debug workflow
    
    * debug workflow
    
    * debug workflow
    
    * debug workflow
    
    * swap to windows
    
    * revert pre-commit trigger
    
    * revert pre-commit trigger
    
    * mark draft

--- src/como_recipes/_interactive_console_builder.py
+++ src/como_recipes/_interactive_console_builder.py
@@ -8,11 +8,3 @@ from como_recipes._command_line_interface import _como_recipes_command_line_inte
 
 if __name__ == "__main__":
     _como_recipes_command_line_interface_main_entrypoint()
-
-    test = "adfsdfsf"
-    print(
-        test
-        + "abcaedfsdfdsfsfsfdsfsfdsfsfdsfsdf"
-        + "143563546435635634563456436346534636534653423"
-        + "354535533453453534b53454535345"
-    )
