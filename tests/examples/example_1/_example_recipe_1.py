be32364f1930da9d8a97671f9b5455611e6062d5
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-11-29 22:26:22 -0500

    Update _example_recipe_1.py

--- tests/examples/example_1/_example_recipe_1.py
+++ tests/examples/example_1/_example_recipe_1.py
@@ -6,6 +6,7 @@ from ..._recipe_registration import default_recipe_registry
 
 class ExampleRecipe1(Recipe):
     name: str = "Example Recipe 1"
+    tags: tuple[str, ...] = ("Italian", "Pasta", "Entree", "Vegetarian")
     measurements: tuple[Measurement, ...] = (
         MeasurementRegistry.get_measurement(amount=3.0, unit="tbsp.", name="ingredient 1"),
         MeasurementRegistry.get_measurement(amount=4.0, unit="g", name="ingredient 2"),
