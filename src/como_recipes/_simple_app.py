6aaa39bf76fb9c84483fe7d801cfcfb776e0e90f
Cody Baker <cody.c.baker.phd@gmail.com>
2024-12-02 17:39:20 -0500

    add basic logic

--- src/como_recipes/_simple_app.py
+++ src/como_recipes/_simple_app.py
@@ -1,6 +1,11 @@
 import tkinter
 
+import natsort
+
 import como_recipes
+
+# TODO: could improve the laziness/performance of add/remove operations w.r.t. current search query instead of
+# regenerating entire search every time
 
 
 class SimpleCoMoApp(tkinter.Tk):
@@ -8,32 +13,78 @@ class SimpleCoMoApp(tkinter.Tk):
         """A very simple GUI implementation for the CoMo Meal Selection based on Tkinter."""
         super().__init__()
 
-        top_banner = tkinter.Label(self, text="Welcome to the CoMo Meal Selection App!")
-        top_banner.pack()
+        self.top_banner = tkinter.Label(self, text="Welcome to the CoMo Meal Selection App!")
 
-        self.available_meals = como_recipes.default_recipe_registry.get_all_recipe_names()
+        self.default_available_index_to_meals: dict[int, str] = {
+            index: recipe_name
+            for index, recipe_name in enumerate(
+                natsort.natsorted(seq=como_recipes.default_recipe_registry.get_all_recipe_names()),
+            )
+        }
+        self.default_available_meals_to_index: dict[str, int] = {
+            recipe_name: index for index, recipe_name in self.default_available_index_to_meals.items()
+        }
 
-        available_meals_search = tkinter.Entry(self)
-        available_meals_search.pack()
-        available_meals_search.bind("<KeyRelease>", self.check_key_search)
+        self.currently_available_index_to_meals: dict[int, str] = self.default_available_index_to_meals.copy()
+        self.currently_selected_index_to_meals: dict[int, str] = {}
 
-        self.available_meals_box = tkinter.Listbox(self)
-        self.available_meals_box.pack()
-        self.update_selector(data=self.available_meals)
+        self.currently_available_meals_search = tkinter.Entry(master=self)
+        self.currently_available_meals_search.bind(sequence="<KeyRelease>", func=self.update_smart_meal_search)
 
-    def check_key_search(self, event: tkinter.Event) -> None:
-        value = event.widget.get()
+        self.currently_available_meals_box = tkinter.Listbox(self)
+        self.currently_available_meals_box.insert(0, *self.currently_available_index_to_meals.values())
+        self.currently_available_meals_box.bind(sequence="<Double-Button-1>", func=self.add_selected_meal)
 
-        if value == "":
-            data = self.available_meals
+        self.selected_meals_box = tkinter.Listbox(self)
+        self.selected_meals_box.bind(sequence="<Double-Button-1>", func=self.remove_selected_meal)
+
+        self.warnings_labels = tkinter.Label(master=self, text="Example warning")
+
+        # Organize components on grid
+        self.top_banner.grid(row=0, column=0, columnspan=2)
+        self.currently_available_meals_search.grid(row=1, column=0)
+        self.currently_available_meals_box.grid(row=2, column=0)
+        self.warnings_labels.grid(row=1, column=1, rowspan=2)
+        self.selected_meals_box.grid(row=3, column=0, columnspan=2)
+
+    def update_smart_meal_search(self, event: tkinter.Event) -> None:
+        """
+        Update the list of displayed meals based on the search query.
+
+        Intended to be as 'smart' as possible.
+        """
+        if self.currently_available_meals_search.get() == "":
+            data = self.currently_available_meals
         else:
-            data = [item for item in self.available_meals if value.lower() in item.lower()]
+            value = self.currently_available_meals_search.get()
+            data = [item for item in self.currently_available_meals if value.lower() in item.lower()]
 
-        self.update_selector(data=data)
+        self.currently_available_meals_box.delete(first=0, last="end")
+        self.currently_available_meals_box.insert("end", *data)
 
-    def update_selector(self, *, data: list[str]) -> None:
-        self.available_meals_box.delete(first=0, last="end")
-        self.available_meals_box.insert("end", *data)
+    def add_selected_meal(self, event: tkinter.Event) -> None:
+        """Move a meal from the available list to the selected list."""
+        selected_meal = self.currently_available_meals_box.get(tkinter.ACTIVE)
+        meal_index = self.default_available_meals_to_index[selected_meal]
+
+        self.selected_meals_box.insert("end", selected_meal)
+
+        self.currently_selected_index_to_meals[meal_index] = selected_meal
+        self.currently_available_index_to_meals.pop(meal_index)
+
+        self.update_smart_meal_search(event=event)
+
+    def remove_selected_meal(self, event: tkinter.Event) -> None:
+        """Move a meal from the selected list back to the available list."""
+        selected_meal = self.selected_meals_box.get(tkinter.ACTIVE)
+        meal_index = self.default_available_meals_to_index[selected_meal]
+
+        self.selected_meals_box.delete(tkinter.ACTIVE)
+
+        self.currently_selected_index_to_meals.pop(meal_index)
+        self.currently_available_index_to_meals[meal_index] = selected_meal
+
+        self.update_smart_meal_search(event=event)
 
 
 if __name__ == "__main__":
