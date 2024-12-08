7dfaa0c2191dbf3828bc3d31d417b0837edb4ecf
Cody Baker <cody.c.baker.phd@gmail.com>
2024-12-08 12:30:39 -0500

    add markdown

--- src/como_recipes/_recipes/_pydantic/_sauteed_green_beans.py
+++ src/como_recipes/_recipes/_pydantic/_sauteed_green_beans.py
@@ -13,11 +13,11 @@ class SauteedGreenBeans(Recipe):
         MeasurementRegistry.get_measurement(amount=1, unit="enough", name="salt & pepper"),  # So it can be "to taste"
     )
     instructions: tuple[str, ...] = (
-        "Coat the bottom of a cast iron skillet with olive oil. Warm oil at medium heat."
-        "Add green beans, stirring to coat with olive oil."
-        "Add salt and pepper to taste, and stir again."
-        "Cover cast iron skillet with a lid, and cook for approximately 8 minutes, stirring occasionally."
-        "When green beans are crisping on the outside, reduce heat to low and continue cooking for ~5 minutes."
+        "Coat the bottom of a cast iron skillet with olive oil. Warm oil at medium heat.",
+        "Add green beans, stirring to coat with olive oil.",
+        "Add salt and pepper to taste, and stir again.",
+        "Cover cast iron skillet with a lid, and cook for approximately 8 minutes, stirring occasionally.",
+        "When green beans are crisping on the outside, reduce heat to low and continue cooking for ~5 minutes.",
     )
 
 
