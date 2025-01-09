d0a4b173f478f48c0f28935748433219e22f3bf0
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2025-01-09 15:16:29 -0500

    clean code (#332)

--- src/como_recipes/app/__init__.py
+++ src/como_recipes/app/__init__.py
@@ -3,14 +3,12 @@ from ._shopping_list_frame import ShoppingListFrame
 from ._selected_meals_frame import SelectedMealsFrame
 from ._como_app import CoMoApp
 from ._session_manager_frame import SessionManagerFrame
-from ._raw_ingredient_frame import RawIngredientFrame
 from ._meal_assembler_frame import MealAssemblerFrame
 
 __all__ = [
     "AvailableRecipesFrame",
     "CoMoApp",
     "MealAssemblerFrame",
-    "RawIngredientFrame",
     "SelectedMealsFrame",
     "ShoppingListFrame",
     "SessionManagerFrame",
