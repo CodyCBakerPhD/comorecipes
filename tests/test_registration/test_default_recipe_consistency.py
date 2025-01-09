2835bcd8b605bfc26d3440a02aaacb14220f0fd6
Cody Baker <cody.c.baker.phd@gmail.com>
2025-01-09 14:19:22 -0500

    add test

--- tests/test_registration/test_default_recipe_consistency.py
+++ tests/test_registration/test_default_recipe_consistency.py
@@ -14,3 +14,16 @@ def test_recipe_counts_consistency():
 
     assert markdown_recipe_count == default_recipe_count
     assert html_recipe_count == default_recipe_count
+
+
+def test_all_units_consistent():
+    recipes = [
+        como_recipes.default_recipe_registry.get_recipe(recipe_name=recipe_name)
+        for recipe_name in como_recipes.default_recipe_registry.get_all_recipe_names()
+    ]
+
+    unique_units = {
+        measurement.unit for recipe in recipes for measurement in recipe.measurements if measurement.amount != "enough"
+    }
+
+    assert unique_units == {"grams", "portions"}
