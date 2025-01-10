764ca2205397be118f3a59e9a928136ba820047f
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2025-01-10 12:27:40 -0500

    Fix garlic units (#336)
    * fix garlic units; add test for consistency; break up recipe lines
    
    * fix garlic units; add test for consistency; break up recipe lines

--- tests/test_registration/test_default_recipe_consistency.py
+++ tests/test_registration/test_default_recipe_consistency.py
@@ -1,3 +1,5 @@
+import collections
+import json
 import pathlib
 
 import como_recipes
@@ -16,7 +18,7 @@ def test_recipe_counts_consistency():
     assert html_recipe_count == default_recipe_count
 
 
-def test_all_units_consistent():
+def test_global_units_in_grams_or_portions():
     recipes = [
         como_recipes.default_recipe_registry.get_recipe(recipe_name=recipe_name)
         for recipe_name in como_recipes.default_recipe_registry.get_all_recipe_names()
@@ -27,3 +29,26 @@ def test_all_units_consistent():
     }
 
     assert unique_units == {"grams", "portions"}
+
+
+def test_units_consistent_per_ingredient():
+    recipes = [
+        como_recipes.default_recipe_registry.get_recipe(recipe_name=recipe_name)
+        for recipe_name in como_recipes.default_recipe_registry.get_all_recipe_names()
+    ]
+
+    all_ingredient_occurences_to_units = collections.defaultdict(list)
+    for recipe in recipes:
+        for measurement in recipe.measurements:
+            if measurement.amount == "enough":
+                continue
+
+            all_ingredient_occurences_to_units[measurement.ingredient.name].append(measurement.unit)
+
+    inconsistent_ingredients = {
+        ingredient_name: tuple(unique_units)
+        for ingredient_name, units in all_ingredient_occurences_to_units.items()
+        if len(unique_units := set(units)) != 1
+    }
+    message = f"\n\nInconsistent units found: \n{json.dumps(obj=inconsistent_ingredients, indent=2)}\n"
+    assert not any(inconsistent_ingredients), message
