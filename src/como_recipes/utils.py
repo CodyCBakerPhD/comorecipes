df246286397f912f64049cccbf5da76e14a002bf
Cody Baker <cody.c.baker.phd@gmail.com>
2025-01-16 19:06:39 -0500

    add one extra level to build

--- src/como_recipes/utils.py
+++ src/como_recipes/utils.py
@@ -81,7 +81,7 @@ def get_executable_name() -> str:
 
     Only supports Windows patterns for the time being.
     """
-    platform_name = "_".join(platform.platform().split("-")[:2])
+    platform_name = "_".join(platform.platform().split("-")[:3])
 
     # Resolve an issue with GitHub Actions builds
     corrected_platform_name = platform_name.removesuffix("Server")
